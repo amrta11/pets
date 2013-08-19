3b07edb004278cfaf22b068886b9152b12108a41
amrta11 <amrta.kf@gmail.com>
2013-08-20 00:16:30 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pets::Application.config.secret_key_base = 'd6bf77068f1e8c43bb40002f52cb213e743d8f7ffbf5c46ea5bc73fcb34c4cbcf9881f9110e36ceb527a4c2039f8fce08924404141da63bebe7275f15030e44b'
